f80820cbc1f4fc2c867ad60ff4c0fd81c7c2b6b0
Ollie Stevenson <ollie@stevenson.digital>
2020-03-07 15:00:16 +0000

    [add] scss animation delay loop to navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,22 +5,22 @@ const Navbar = () => {
   return (
     <nav className="Navbar">
       <ul className="Navbar__ul">
-        <li className="Navbar__li">
+        <li className="Navbar__li a-fade-in-up">
           <NavLink className="Navbar__link f-vogue-26-18" activeClassName="is-active" exact={true} to="/">
             Home
           </NavLink>
         </li>
-        <li className="Navbar__li">
+        <li className="Navbar__li a-fade-in-up">
           <NavLink className="Navbar__link f-vogue-26-18" activeClassName="is-active" to="/experience">
             Experience
           </NavLink>
         </li>
-        <li className="Navbar__li">
+        <li className="Navbar__li a-fade-in-up">
           <NavLink className="Navbar__link f-vogue-26-18" activeClassName="is-active" to="/skills">
             Skills
           </NavLink>
         </li>
-        <li className="Navbar__li">
+        <li className="Navbar__li a-fade-in-up">
           <NavLink className="Navbar__link f-vogue-26-18" activeClassName="is-active" to="/contact">
             Contact
           </NavLink>
